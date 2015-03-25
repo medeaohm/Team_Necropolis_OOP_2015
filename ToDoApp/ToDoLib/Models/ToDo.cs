60fbc72e1398cae7fe588c790e4c1814a68dcd5b
puhabg <puhabg@live.com>
2015-03-25 22:24:10 +0200

    Adds skeleton :skull: and SimpleNote

---
+++ ToDoApp/ToDoLib/Models/ToDo.cs
@@ -0,0 +1,13 @@
+﻿// Summary:
+//     Represents a Simple ToDo with tags has a due date
+//     Inherits Siple Note 
+//
+// Type parameters:
+//   string title , string text, List<strings> tags
+namespace ToDoLib.Models
+{
+    class ToDo : SimpleNote
+    {
+
+    }
+}
